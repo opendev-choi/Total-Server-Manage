1ceea22bb9982e9260807c1c1376d950786b3334
JongHyeok Choi <opendev.choi@gmail.com>
2018-07-31 21:00:49 +0900

    Protobuf 관련 툴 추가
    -v0.0.1
    
    + statics.py
    Tool intro 문자열 추가
    
    + tools/protobuf-tool.py
    Protobuf를 쉽게 이용하기 위한 툴

--- statics.py
+++ statics.py
@@ -18,3 +18,11 @@ address = server.elasticsearch
 """
 
 config_elastic_section_name: str = "elasticsearch"
+
+protobuf_tool_intro: str = """
+Protobuf Tool 입니다.
+사용법은 두가지이며, Encode와 Decode 기능입니다.
+
+Encode 시에는 해당하는 정보를 입력하면 Python Byte 형식의 메세지를 출력하게 됩니다.
+Decode 시에는 b'lorem ipsum' 식의 정보를 입력하면 데이터를 출력합니다.
+"""
